004ea2b7be7ca8030b1f65899fbfe3346a033d92
Claude Meurehg <claude.meurehg@mail.utoronto.ca>
2018-06-12 13:07:07 -0400

    links to images completed

---
+++ assets/functions.js
@@ -0,0 +1,8 @@
+
+
+//var imageLink = document.getElementById("bootcamp-tag");
+
+
+//document.getElementById("bootcamp-tag").onclick = imageLink();
+
+//imageLink.addEventListener("click", href = "https://cmeurehg.github.io/unit-4-game");
